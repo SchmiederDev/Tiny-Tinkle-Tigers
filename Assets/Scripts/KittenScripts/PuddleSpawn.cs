f2ba7b519279e108a64ed3eb68a792f8549bfec3
Robert Schmieder <robert.schmieder87@gmx.de>
2023-06-20 08:56:12 +0200

    Added a line of code which updates the 'PeePuddlesOnScene'-List of 'TheGame.GameControl'*
    *: they are added after instantiation

--- Assets/Scripts/KittenScripts/PuddleSpawn.cs
+++ Assets/Scripts/KittenScripts/PuddleSpawn.cs
@@ -22,14 +22,20 @@ public class PuddleSpawn : MonoBehaviour
     IEnumerator SpawnFirstPuddle(float momentOfFirstPuddleSpawn)
     {
         yield return new WaitForSeconds(momentOfFirstPuddleSpawn);
+        
         GameObject PeePuddleInstance = Instantiate(PeePuddle, gameObject.transform.position, Quaternion.identity);
+        TheGame.GameControl.PuddlesOnScene.Add(PeePuddleInstance);
+
         StartCoroutine(SpawnPuddle());
     }
 
     IEnumerator SpawnPuddle()
     {
         yield return new WaitForSeconds(puddleSpawnRate);
+        
         GameObject PeePuddleInstance = Instantiate(PeePuddle, gameObject.transform.position, Quaternion.identity);
+        TheGame.GameControl.PuddlesOnScene.Add(PeePuddleInstance);
+
         StartCoroutine(SpawnPuddle());
     }
 }
